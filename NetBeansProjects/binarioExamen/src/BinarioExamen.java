c5e6c14701f33ebd15aecbf5cba5784bba3918a0
Santiago <santiago.contactoyt@gmail.com>
2023-11-28 15:08:41 +0100

    Este es el examen resuelto de verdad.

--- NetBeansProjects/binarioExamen/src/BinarioExamen.java
+++ NetBeansProjects/binarioExamen/src/BinarioExamen.java
@@ -2,11 +2,13 @@
 import java.util.Scanner;
 
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nb     host/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
+
 //Programa Java para pasar de decimal a binario
 //el programa lee un número entero que se le pide al usuario y muestra su valor en sistema binario(0,1)
+
 /**
  *
  * @author alumnot
@@ -16,29 +18,34 @@ public class BinarioExamen {
     //Método para pasar de decimal a binario
     public static void decBin(int n) {
 
-        if (n <= 2) {
+        if (n < 2) {
+            //equitado el = para que sea menor estricto
             System.out.print(n);
         } else {
             decBin(n / 2);
-            System.out.print(n ^ 2);
+            System.out.print(n % 2);
+            //cambié el ^ por % 
         }
     }
-
-    // NO recuerdo como convertirlo a codigo binario lo siento
-    
-    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n;  // "n" no necesita estar inicializada
 
         //int i = 0; la variable i no es necesaria
         do {
-            System.out.print("Introduzca un número que sea mayor que 0: ");
+            System.out.print("Introduzca un número (mayor a 0): ");
             n = sc.nextInt();
-        } while (n < 0);
+        } while (n < 1);
+        //quité el igual y cambié el 0 por 1
 
         System.out.print("\nBinario: ");
         decBin(n);
         System.out.println("");
+        
+        /*
+          Use el Debug con F7 para ir pasando paso por paso cada linea de codigo
+          y fallaba ^ en el metodo decBIn y lo cambié por % que así si sería
+          correcto
+        */
     }
 }
